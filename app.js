0928e2fce4d801fe3e4509cd38aa475ebcc20c7a
Hubbil Haqi <hubbilhaqi1@gmail.com>
2026-06-01 01:28:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use('/potential-partners', require('./routes/potentialPartner'));
 app.get('/', (req, res) => res.redirect('/auth/login'));
 app.use('/follow-up', require('./routes/followUp'));
 app.use('/mou', require('./routes/mou'));
+app.use('/api', require('./routes/api'));
+app.use('/export', require('./routes/export'));
 
 app.listen(3000, () => {
   console.log('Server berjalan di http://localhost:3000');
